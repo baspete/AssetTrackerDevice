620ffeaf78e8aeac23fb8387bc56a4b230ab9e85
Pete Butler <pete.butler@ge.com>
2019-02-27 16:09:39 -0800

    initial commit

---
+++ src/AssetTrackerDevice.cpp
@@ -0,0 +1,23 @@
+#include "application.h"
+#line 1 "/Users/212320392/workspace/pete/AssetTrackerDevice/src/AssetTrackerDevice.ino"
+/*
+ * Project AssetTrackerDevice
+ * Description:
+ * Author: pbutler@basdesign.com
+ * Date: 2019-02-27
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/212320392/workspace/pete/AssetTrackerDevice/src/AssetTrackerDevice.ino"
+void setup()
+{
+  // Put initialization like pinMode and begin functions here.
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+  // The core of your code will likely live here.
+}
